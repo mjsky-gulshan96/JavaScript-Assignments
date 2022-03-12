0b726c01630155f3acd1a80981fd3a070370bb14
mjsky-gulshan96 <gulshan1212kumar@gmail.com>
2022-03-12 10:33:06 +0530

    modified calculator

--- Calculator/domLinking.js
+++ Calculator/domLinking.js
@@ -4,13 +4,50 @@ let symbol = document.querySelectorAll('.operators button, #percent'); // for op
 let compute = document.querySelector("#compute"); // for equal button
 let clearAll = document.querySelector("#clearAll"); // for AC btn
 let remove = document.querySelector("#remove"); // for back key
+let powerBtn = document.getElementById('on_off');
+let buttons = document.querySelectorAll('button');
 
-import calculator from "./CalculatorClass.js";  
+import calculator from "./CalculatorClass.js";
 import calculate from "./CalculatorFunction.js";
+
+for (const btn of buttons) {
+    if (btn.id === 'on_off') {
+        continue;
+    }
+    btn.disabled = true;
+}
+
+powerBtn.addEventListener('click', function () {
+    if (powerBtn.innerText === 'ON') {
+        powerBtn.innerText = 'OFF';
+        for (const btn of buttons) {
+            btn.style.backgroundColor = '#666666'
+            if (btn.id === 'on_off') {
+                continue;
+            }
+            btn.disabled = false;
+        }
+        let display = document.getElementById('display');
+        display.setAttribute('style', 'background-color:#8fb3a3; color: #3b3b3b;')
+    } else {
+        powerBtn.innerText = 'ON';
+
+        for (const btn of buttons) {
+            btn.style.backgroundColor = '#3b3b3b'
+            if (btn.id === 'on_off') {
+                continue;
+            }
+            btn.disabled = true;
+        }
+        document.getElementById('display').style.backgroundColor = '#3b3b3b';
+    }
+})
 
 // on numbers 
 for (const digit of digits) {
-
+    if(digit.id==='on_off') {
+        continue;
+    }
     digit.addEventListener('click', function () {
         let digitValue = digit.innerHTML;
         calculator.appendNumber(digitValue);
@@ -43,7 +80,7 @@ compute.addEventListener('click', function () {
     const array = calculator.equalTo();
     if (array === undefined) return;
     // sending the array for calculator in calculatorFunction
-    let val=calculate(array);
+    let val = calculate(array);
 
     // updating the result returned from calculate function
     document.querySelector('#display').value = val;
